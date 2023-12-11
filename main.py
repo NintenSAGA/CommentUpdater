b8e069817c43bd7b6ee8b395bf3b0ed4c4258923
Ziyue Tan <201250093@smail.nju.edu.cn>
2023-12-11 21:51:17 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.llms import Ollama
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+llm = Ollama(
+    model="llama2", callback_manager=CallbackManager([StreamingStdOutCallbackHandler()])
+)
+
+old_method = ('private String getSessionFileName(String sessionIdentifier , boolean createSessionFolder) {File '
+              'sessionFolder = folders.get(sessionIdentifier , createSessionFolder); return new File(sessionFolder , '
+              '" data" ). getAbsolutePath (); }')
+new_method = ('private String getSessionFileName(String sessionIdentifier) {File sessionFolder = folders.get('
+              'sessionIdentifier , true); return new File(sessionFolder , " data" ). getAbsolutePath (); }')
+old_comment = ('If the session folder (folder that contains the ﬁle) does not exist and createSessionFolder is true, '
+               'the folder will be created.')
+
+source = '''
+Old Method: ```{old_method}```
+
+New Method: ```{new_method}```
+
+Old Comment: ```{old_comment}```
+
+Please infer the new comment according to the code modification
+
+Requirement: 
+    1. No explanation. No natural words.
+    2. Perform the minimum modification
+    3. Avoid any lose of information
+'''
+
+prompt = ChatPromptTemplate.from_template(source)
+output_parser = StrOutputParser()
+
+chain = prompt | llm | output_parser
+
+chain.invoke({
+    'new_method': new_method,
+    'old_method': old_method,
+    'old_comment': old_comment,
+})
